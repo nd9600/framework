830af2e95aee35bae6e38bc7313570d18414720c
Nathan Douglas <nd9600@gmail.com>
2018-05-01 21:46:51 +0100

    changed config

--- config.r
+++ config.r
@@ -5,17 +5,16 @@ Rebol [
 ; to access the public_dir, start the url with /public/
 ; 'public_prefix must have a / at the end
 ; file!s don't need to have one
-config: make object! [
-    port: 8000
 
-    routing_dir: %routing/
-    route_files: [%routes.r]
+port: 8000
 
-    controllers_dir: %controllers/
-    templates_dir: %templates/    
+routing_dir: %routing/
+route_files: [%routes.r]
 
-    storage_dir: %storage/
+controllers_dir: %controllers/
+templates_dir: %templates/    
 
-    public_dir: %storage/public/
-    public_prefix: "/public/"
-]
+storage_dir: %storage/
+
+public_dir: %storage/public/
+public_prefix: "/public/"
